445083ab02bf02ee0acf67484a68e5b17727ef0c
Amka Tsendjargal <amka150@gmail.com>
2024-01-15 21:29:22 -0700

    typos

--- app/AboutMe.jsx
+++ app/AboutMe.jsx
@@ -15,7 +15,7 @@ export default function AboutMe() {
                     <h1 className="text-base xl:text-3xl font-medium">I am a software developer based in</h1>
                     <h1 className="text-base xl:text-3xl font-medium mb-4">Calgary, Alberta.</h1>
                     <h1 className="text-sm md:text-base xl:text-2xl mb-4">My previous background is in Astrophysics from the University of Calgary, which is also where I was introduced to programming for the first time.</h1>
-                    <h1 className="text-sm md:text-base xl:text-2xl mb-4">Now I&apos;m enrolled in the Object-Oriented Software Development program at SAIT, <strong>currently looking for internship and junior roles.</strong> In my free time, I&apos;m either working on personal project or planning the next one.</h1>
+                    <h1 className="text-sm md:text-base xl:text-2xl mb-4">Now I&apos;m enrolled in the Object-Oriented Software Development program at SAIT, <strong>currently looking for internships and junior roles.</strong> In my free time, I&apos;m either working on personal projects or planning the next one.</h1>
                     <h1 className="text-sm md:text-base xl:text-2xl mb-10 md:mb-4">When I&apos;m not at the computer, I&apos;m usually playing volleyball or being a nuiscence to my dog.</h1>
                     <div className="md:hidden">
                         <ExternalLinks />
